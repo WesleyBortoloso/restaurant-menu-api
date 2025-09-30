e4f1c8c9b476c004968ac43155d134a06725dddb
WesleyBortoloso <wesleybortolosocco@gmail.com>
2025-09-30 19:04:20 -0300

    feat: create menus model

---
+++ spec/models/menu_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Menu, type: :model do
+  describe 'associations' do
+    it { should have_many(:menu_items).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    subject { build(:menu) }
+
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'custom scenarios' do
+    it 'is valid with a name' do
+      menu = build(:menu, name: 'Lunch')
+      expect(menu).to be_valid
+    end
+
+    it 'is invalid without a name' do
+      menu = build(:menu, name: nil)
+      expect(menu).not_to be_valid
+      expect(menu.errors[:name]).to include("can't be blank")
+    end
+  end
+end
