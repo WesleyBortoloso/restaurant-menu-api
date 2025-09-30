8d70824a28afe1ab158b733c2f5a367c57b2df77
WesleyBortoloso <wesleybortolosocco@gmail.com>
2025-09-30 19:02:29 -0300

    chore: enable simplecov and factorybot

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require "simplecov"
 
-SimpleCov.start
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
