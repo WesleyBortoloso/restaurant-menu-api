8cf038c9285bdccb7967909968980b071130599e
WesleyBortoloso <wesleybortolosocco@gmail.com>
2025-09-30 21:48:40 -0300

    refactor: change menu items endpoints

--- app/api/v1/menu_items.rb
+++ app/api/v1/menu_items.rb
@@ -2,14 +2,29 @@ module V1
   class MenuItems < Grape::API
     resource :menu_items do
 
-      desc 'List menu items for a menu'
+      desc 'List all menu items'
+      get do
+        menu_items = MenuItem.all
+        MenuItemSerializer.new(menu_items).serializable_hash
+      end
+
+      desc 'Show a menu item'
+      params do
+        requires :id, type: Integer
+      end
+      get ':id' do
+        item = MenuItem.find(params[:id])
+        MenuItemSerializer.new(item).serializable_hash
+      end
+
+      desc 'List all menus for a menu item'
       params do
         requires :menu_id, type: Integer, desc: 'Menu ID'
       end
-      get do
-        menu = Menu.find(params[:menu_id])
-        items = menu.menu_items
-        MenuItemSerializer.new(items).serializable_hash
+      get ':menu_id/menus' do
+        menu_item = MenuItem.find(params[:menu_id])
+        menus = menu_item.menus
+        MenuSerializer.new(menus).serializable_hash
       end
     end
   end
--- spec/api/v1/menu_items_spec.rb
+++ spec/api/v1/menu_items_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'V1::MenuItems API', type: :request do
   let!(:menu) { create(:menu) }
-  let!(:menu_items) { create_list(:menu_item, 3, menu: menu) }
+  let!(:menu_items) do
+    create_list(:menu_item, 3).each { |item| menu.menu_items << item }
+  end
 
   describe 'GET /api/v1/menu_items' do
-    context 'when menu exists with items' do
-      before { get "/api/v1/menu_items", params: { menu_id: menu.id } }
+    context 'when exists menu items' do
+      before { get "/api/v1/menu_items" }
 
-      it 'returns all menu items for the menu' do
+      it 'returns all menu items' do
         json = JSON.parse(response.body)
         expect(json['data'].size).to eq(3)
-        expect(json['data'].first['attributes']['name']).to eq(menu_items.first.name)
       end
 
       it 'returns status 200' do
@@ -19,10 +20,8 @@ RSpec.describe 'V1::MenuItems API', type: :request do
       end
     end
 
-    context 'when menu exists but has no items' do
-      let!(:empty_menu) { create(:menu) }
-
-      before { get "/api/v1/menu_items", params: { menu_id: empty_menu.id } }
+    context 'when no menu items exists' do
+      before { Menu.destroy_all; get '/api/v1/menus' }
 
       it 'returns an empty array' do
         json = JSON.parse(response.body)
@@ -33,17 +32,53 @@ RSpec.describe 'V1::MenuItems API', type: :request do
         expect(response).to have_http_status(200)
       end
     end
+  end
 
-    context 'when menu does not exist' do
-      before { get "/api/v1/menu_items", params: { menu_id: 99999 } }
+  describe 'GET /api/v1/menu_items/:id' do
+    context 'when menu item exists' do
+      let(:item) { menu_items.first }
+      before { get "/api/v1/menu_items/#{item.id}" }
+
+      it 'returns the menu item' do
+        json = JSON.parse(response.body)
+        expect(json['data']['id']).to eq(item.id.to_s)
+        expect(json['data']['attributes']['name']).to eq(item.name)
+      end
+
+      it 'returns status 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when menu item does not exist' do
+      before { get "/api/v1/menu_items/99999" }
 
       it 'returns status 404' do
         expect(response).to have_http_status(404)
       end
+    end
+  end
 
-      it 'returns a JSON error message' do
+  describe 'GET /api/v1/menu_items/:menu_item_id/menus' do
+    context 'when menu item exists and has menus' do
+      let(:item) { menu_items.first }
+      before { get "/api/v1/menu_items/#{item.id}/menus" }
+
+      it 'returns all menus for the menu item' do
         json = JSON.parse(response.body)
-        expect(json['message']).to eq("Couldn't find Menu with 'id'=99999")
+        expect(json['data'].first['id']).to eq(menu.id.to_s)
+      end
+
+      it 'returns status 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when menu item does not exist' do
+      before { get "/api/v1/menu_items/99999/menus" }
+
+      it 'returns status 404' do
+        expect(response).to have_http_status(404)
       end
     end
   end
